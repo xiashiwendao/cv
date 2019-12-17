caf38161a600fd369a5349b34470bb2fc09328e3
xiashiwendao <xiashiwendao@163.com>
2019-12-17 15:00:45 +0800

    implement task5 code, next need to run it

--- homework/homework1/task5/task5.py
+++ homework/homework1/task5/task5.py
@@ -30,7 +30,6 @@ def generate_caption(model, tokenizer, photo_feature, max_length = 40):
     """
     in_text = 'startseq'
     for i in range(max_length):
-
         sequence = tokenizer.texts_to_sequences([in_text])
         sequence = pad_sequences(sequence, maxlen=max_length)
         output = model.predict([photo_feature, sequence])
@@ -43,6 +42,29 @@ def generate_caption(model, tokenizer, photo_feature, max_length = 40):
             break
 
     return in_text
+
+
+def generate_caption_run():
+    image_file = 'Flickr_8k.testImages.txt'
+    ids = util.load_ids(image_file)
+    features = util.load_photo_features('feature.pkl', ids)
+
+    nlp_model = load_model('model1.h5')
+    tokenizer = load(open('tokenizer.plk', 'rb'))
+
+    generate_caption(nlp_model, tokenizer, features['3596131692_91b8a05606'], 40)
+
+
+
+def evluate_test():
+    from nltk.translate.bleu_score import corpus_bleu
+    references = [[['there', 'is','a','cat','and','a','dog']]]
+    candidates = [['there', 'is', 'a', 'cat', 'and','a','pig']]
+    score = corpus_bleu(references, candidates, weights=(1,0,0,0))
+    print(score)
+
+evluate_test()
+
 
 
 def evaluate_model(model, captions, photo_features, tokenizer, max_length = 40):
@@ -63,9 +85,32 @@ def evaluate_model(model, captions, photo_features, tokenizer, max_length = 40):
             第四个元素为权重为(0.25, 0.25, 0.25, 0.25)的ＢＬＥＵ分数
 
     """
-    pass
+    score_1 = 0
+    score_2 = 0
+    score_3 = 0
+    score_4 = 0
+    for key in photo_features:
+        feature = photo_features[key]
+        caption_raw = captions[key]
+        caption_generated = generate_caption(model, tokenizer, feature)
+        score_1 += corpus_bleu([[[caption_raw]]], [[caption_generated]], weights=[1.0, 0, 0, 0])
+        score_2 += corpus_bleu([[[caption_raw]]], [[caption_generated]], weights=[0.5, 0.5, 0, 0])
+        score_3 += corpus_bleu([[[caption_raw]]], [[caption_generated]], weights=[0.3, 0.3, 0.3, 0])
+        score_4 += corpus_bleu([[[caption_raw]]], [[caption_generated]], weights=[0.25, 0.25, 0.25, 0.25])
+    total_size = len(photo_features)
+    score_1 /= total_size
+    score_2 /= total_size
+    score_3 /= total_size
+    score_4 /= total_size
+
+    return score_1, score_2, score_3, score_4
 
 if __name__ == '__main__':
     print('main!')
     print(sys.argv)
 
+dict = dict()
+dict["1"] = '1'
+dict["2"] = '2'
+dict["3"] = '3'
+len(dict)
